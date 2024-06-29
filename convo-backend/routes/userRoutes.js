b9f6d6ff1753014a71dbc81b11332b8d7460bd40
marouaneK02 <152206957+marouaneK02@users.noreply.github.com>
2024-06-29 19:48:20 +0100

    added post routes to backend

---
+++ convo-backend/routes/userRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { signupUser, loginUser, logoutUser, followUnfollowUser, updateUser } from "../controllers/userController.js";
+import protectRoute from "../middlewares/protectRoute.js";
+
+const router = express.Router();
+
+router.post("/signup", signupUser);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+router.post("/follow/:id", protectRoute, followUnfollowUser);
+router.post("/update/:id", protectRoute, updateUser);
+
+export default router;
---
+++ convo-backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/ConnectDB.js";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/userRoutes.js"
+
+dotenv.config();
+
+connectDB();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Routes
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));

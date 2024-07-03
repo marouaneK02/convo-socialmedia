a8fc18982d23f2307d9d66856d60d8a2735065ce
marouaneK02 <152206957+marouaneK02@users.noreply.github.com>
2024-07-03 21:59:49 +0100

    started login and signup pages

--- convo-frontend/src/App.jsx
+++ convo-frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom"
 import UserPage from "./pages/UserPage"
 import PostPage from "./pages/PostPage"
 import Header from "./components/Header"
+import HomePage from "./pages/HomePage"
+import AuthPage from "./pages/AuthPage"
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
     <Container maxW = "620px">
       <Header/>
       <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/auth" element={<AuthPage/>}/>
         <Route path="/:username" element={<UserPage/>}/>
         <Route path="/:username/post/:pid" element={<PostPage/>}/>
       </Routes>
---
+++ convo-frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,13 @@
+import LoginCard from "../components/LoginCard"
+import SignupCard from "../components/SignupCard"
+
+
+const AuthPage = () => {
+  return (
+    <>
+        <SignupCard/>
+    </>
+  )
+}
+
+export default AuthPage

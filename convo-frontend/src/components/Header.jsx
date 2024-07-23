878b8ea9fb411da2639c03f538a3ce110cb07d01
marouaneK02 <152206957+marouaneK02@users.noreply.github.com>
2024-07-23 19:01:01 +0100

    started chat page UI

--- convo-frontend/src/components/Header.jsx
+++ convo-frontend/src/components/Header.jsx
@@ -6,6 +6,7 @@ import { Link as RouterLink } from "react-router-dom";
 import { RxAvatar } from "react-icons/rx";
 import { FiLogOut } from "react-icons/fi";
 import useLogout from "../hooks/useLogout";
+import { BsFillChatQuoteFill } from "react-icons/bs";
 
 const Header = () => {
   const {colorMode} = useColorMode();
@@ -27,6 +28,9 @@ const Header = () => {
           <Flex alignItems={"center"} gap={"4"}>
             <Link as={RouterLink} to={`/${user.username}`}>
               <RxAvatar size={"24"} />
+            </Link>
+            <Link as={RouterLink} to={`/chat`}>
+              <BsFillChatQuoteFill size={"24"} />
             </Link>
             <Button size={"xs"} onClick={logout}>
               <FiLogOut size={"20"}/>
---
+++ convo-frontend/src/pages/ChatPage.jsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const ChatPage = () => {
+  return (
+    <Box position={"absolute"} left={"50%"} transform={"translateX(-50%)"}>
+    </Box>
+  )
+}
+
+export default ChatPage
